283b8826ad2bb7c2baab003a029fed42655f05ef
Phop <aran.kochhiran@gmail.com>
2024-03-08 15:00:51 +0700

    Check container height and line height when the window resizing

--- src/components/TextCollapse.tsx
+++ src/components/TextCollapse.tsx
@@ -1,9 +1,10 @@
 import { useCallback, useEffect, useRef, useState } from "react"
 
+const wrapClassName = 'overflow-hidden whitespace-nowrap text-clip';
 
 function TextCollapse({ text }: { text: string }) {
   const [isMultiline, setIsMultiline] = useState(false);
-  const textRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
   const [isActive, setIsActive] = useState<boolean>(false);
 
   const handleActive = useCallback(() => {
@@ -12,8 +13,17 @@ function TextCollapse({ text }: { text: string }) {
 
   useEffect(() => {
     const handleResize = () => {
-      if (textRef.current && textRef.current.clientHeight > 25) {
-        setIsMultiline(true);
+      if (containerRef.current) {
+        containerRef.current.className = containerRef.current.className.replace(wrapClassName, 'normal');
+        const computedStyle = window.getComputedStyle(containerRef.current);
+        const lineHeight = parseInt(computedStyle.getPropertyValue('line-height').replace('px', ''));
+        const height = parseInt(computedStyle.getPropertyValue('height').replace('px', ''));
+
+        //Computed from container's height and front size to make sure this feature still working if font size changed
+        //Also padding if needed
+        (height > (lineHeight + (lineHeight / 2))) ? setIsMultiline(true) : setIsMultiline(false);
+
+        containerRef.current.className = containerRef.current.className.replace('normal', wrapClassName);
       } else {
         setIsMultiline(false);
       }
@@ -28,16 +38,14 @@ function TextCollapse({ text }: { text: string }) {
 
   const activeText = isActive ? '...See Less' : '...See More'
   const activeClassName = isActive ? '' : 'absolute right-0'
-  const textClassName = isActive ? '' : isMultiline ? 'overflow-hidden whitespace-nowrap text-clip' : ''
+  const textClassName = isActive ? '' : isMultiline ? wrapClassName : ''
 
   return (
-    <div>
-      <div className={`relative max-w-full ${textClassName}`} ref={textRef}>
-        {text}
-        <span hidden={!isMultiline} className={`text-[#72BFFF] bg-white cursor-pointer select-none ${activeClassName}`} onClick={handleActive}>
-          {activeText}
-        </span>
-      </div>
+    <div className={`relative max-w-full ${textClassName}`} ref={containerRef}>
+      <span>{text}</span>
+      <span hidden={!isMultiline} className={`text-[#72BFFF] bg-white cursor-pointer select-none ${activeClassName}`} onClick={handleActive}>
+        {activeText}
+      </span>
     </div>
   )
 }

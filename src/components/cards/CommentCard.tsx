5d05ac898e09496f9d75624cd66bdae16190aa39
Phop <aran.kochhiran@gmail.com>
2024-03-08 15:24:40 +0700

    Adjust styling of post cardd and comment card

--- src/components/cards/CommentCard.tsx
+++ src/components/cards/CommentCard.tsx
@@ -1,10 +1,9 @@
 import { CommentData } from "../../datas/commentData"
 import TextCollapse from "../TextCollapse"
 
-
 function CommentCard({ data }: { data: CommentData }) {
   return (
-    <div>
+    <div className="bg-white drop-shadow p-5 rounded-lg">
       <b>{data.email}</b>
       <TextCollapse text={data.body} />
     </div>

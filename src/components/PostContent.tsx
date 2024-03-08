532369d9cb29065f020538f6a25dc50210cf0486
Phop <aran.kochhiran@gmail.com>
2024-03-08 15:08:25 +0700

    Add textarea and padding bottom of post card

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -7,6 +7,11 @@ function PostContent({ data }: { data: PostData }) {
       <h2>{data.title}</h2>
       <p>{data.body}</p>
       <CommentList />
+      <textarea
+        id="message"
+        rows={4}
+        className="block p-2.5 w-full text-sm rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-400 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Textarea"
+      />
     </>
   )
 }
